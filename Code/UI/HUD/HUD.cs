3dbb5fddb675e03f44c645e21768b433d6e4cd41
Sam Poirier <sampoirier@email.com>
2025-03-08 15:29:40 +0000

    Add crosshair dot

---
+++ Code/UI/HUD/HUD.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox.Rendering;
+
+namespace Spring.UI.HUD
+{
+    public class HUD : Component
+	{
+
+		[Property, Category("Crosshair"), Title("Draw?")] 
+		bool mDrawCrosshair = true;
+		[Property, Category("Crosshair"), Title("Size")]
+		int mCrosshairSize = 4;
+		protected override void OnUpdate()
+		{
+			if (Scene.Camera == null)
+				return;
+
+			HudPainter hud = Scene.Camera.Hud;
+
+			if (mDrawCrosshair)
+				hud.DrawCircle( new Vector2( Screen.Width * 0.5f, Screen.Height * 0.5f ), mCrosshairSize, Color.White );
+		}
+    }
+}
